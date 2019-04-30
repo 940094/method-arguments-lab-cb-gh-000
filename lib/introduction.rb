58b2be78f5a6982a92e0e3cd4e1c313a228d226a
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 14:35:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  puts "Hello #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "hello #{justin} who speaks #{language}"
+  puts "#Hi, my name is #{name} and I am learning to program in #{language}."
 end
